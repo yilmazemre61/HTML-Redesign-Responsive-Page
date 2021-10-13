e149d7b314d2b5c4bd926d9631935ee157d7add6
Emre <61523720+yilmazemre61@users.noreply.github.com>
2021-10-13 12:42:10 +0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    // adding the navigation
+    $(function(){
+        $("#navigation").html('<div><a href="index.html"><img src="../images/logo.png" alt="our logo"/></a></div><nav><div><ul><li><a href="index.html">Home</a></li><li><a href="#">About</a></li><li><a href="#">Contact</a></li></ul></div></nav>');
+    });
+    // adding the footer
+    $(function(){
+        $("#footer").html('<div><p>Assignment By: Emre YILMAZ &amp; 200471130</p><div>');
+    });
+});
